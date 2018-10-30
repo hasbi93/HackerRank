238c1f48d414d8e08ce33f7e1cfec85ea1720b4c
clfm <clfm.public@gmail.com>
2018-10-30 16:54:31 -0600

    Add solutions for 30 Days of Code, Days 0-4 (Python)

---
+++ 30 Days of Code/Day 02 - Operators/solution.py
@@ -0,0 +1,11 @@
+def solve(meal_cost, tip_percent, tax_percent):
+    tip_total = meal_cost * tip_percent / 100
+    tax_total = meal_cost * tax_percent / 100
+    total = meal_cost + tip_total + tax_total
+    return round(total)
+
+meal_cost = float(input())
+tip_percent = int(input())
+tax_percent = int(input())
+
+print(solve(meal_cost, tip_percent, tax_percent))
---
+++ 30 Days of Code/Day 04 - Class vs. Instance/solution.py
@@ -0,0 +1,39 @@
+class Person:
+    # Inclusive limits for the defined age ranges.
+    YOUNG_MAX = 12
+    TEEN_MIN = 13
+    TEEN_MAX = 17 # Interesting definition of "teen"...
+    
+    def __init__(self, initialAge):
+        if initialAge < 0:
+            print("Age is not valid, setting age to 0.")
+            self.age = 0
+        else:
+            self.age = initialAge
+        
+    def amIOld(self):
+        answer = ""
+        if self.age in range(Person.YOUNG_MAX + 1):
+            answer = "You are young."
+        elif self.age in range(Person.TEEN_MIN, Person.TEEN_MAX + 1):
+            answer = "You are a teenager."
+        else:
+            answer = "You are old."
+        print(answer)
+        
+    def yearPasses(self):
+        self.age += 1
+        
+################################################################################
+#----------------------------- begin locked code ------------------------------#
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
+#------------------------------ end locked code -------------------------------#
+################################################################################
