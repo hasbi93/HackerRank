b968c5d80d267718052e61065ebdffa553dcc44a
clfm <clfm.public@gmail.com>
2019-01-09 00:12:59 -0700

    Add C language solutions

--- C/Arrays and Strings/Digit Frequency/solution.c
+++ C/Arrays and Strings/Digit Frequency/solution.c
@@ -1,13 +1,28 @@
-/* ************************************************************************** */
-/* --------------------------- begin locked code ---------------------------- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-/* ---------------------------- end locked code ----------------------------- */
-/* ************************************************************************** */
+#define MAXLINE 1000
 
+void get_digit_freqs(char *string, int *digit_freqs) {
+    char *c = string;
+    while (*c != '\0') {
+        if (isdigit(*c)) {
+            digit_freqs[*c - '0'] += 1;
+        }
+        c++;
+    }
+}
 
+int main(void) {
+    char string[MAXLINE];
+    fgets(string, MAXLINE, stdin);
 
-/* ************************************************************************** */
-/* --------------------------- begin locked code ---------------------------- */
+    int *digit_freqs = (int *) calloc(10, sizeof(int));
+    get_digit_freqs(string, digit_freqs);
 
-/* ---------------------------- end locked code ----------------------------- */
-/* ************************************************************************** */
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", digit_freqs[i]);
+    }
+    printf("\n");
+}
